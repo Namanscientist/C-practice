c2f62936229c5ec291b9f6d01f1219366d2ca615
Naman <namansharma2005206@gmail.com>
2025-02-24 10:18:49 +0530

    Add files via upload

---
+++ learning.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//SKELETON OF C PROGRAMMING:-
+int main() {
+    printf("Hello world \n ");
+
+//VARIABLES:-
+    char symbol = '*';
+    char name [] = "naman";
+    int age = 19;
+    float height = 5.9;
+
+//OUTPUT :-
+    printf("%c\n", symbol);//Output of single char 
+    printf("%s\n", name);//Output of multi-char
+    printf("%d\n", age);//Output of integer 
+    printf("%f\n", height);//Output of float
+
+//COMMENTS:-
+
+// --> USED FOR SINGLE LINE COMMENTS.
+/* USED FOR 
+MULTI - LINE COMMENTS*/
+
+    return 0;
+
+}
